3f39c75972c7ce0a9361f3787da56ded6c4c82c0
DevaAgulla <87611913+DevaAgulla@users.noreply.github.com>
2024-06-07 23:41:12 +0530

    Create firstMissingPositiveCycleSort.cpp

---
+++ leetcodeHard/firstMissingPositiveCycleSort.cpp
@@ -0,0 +1,12 @@
+ int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            while(nums[i]>=1 and nums[i]<=n and nums[nums[i]-1]!=nums[i]){
+                swap(nums[nums[i]-1],nums[i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1) return i+1;
+        }
+        return n+1;
+    }
